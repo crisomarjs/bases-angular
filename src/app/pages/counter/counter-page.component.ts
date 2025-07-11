cd0a906eaa143f88892ea2192c392505cf8c0744
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-07-11 17:36:16 -0600

    Carga de condigo

---
+++ src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styles: `
+    button {
+      padding: 5px;
+      margin: 5px 10px;
+      width: 75px;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CounterPageComponent {
+
+  counter = 10;
+  counterSignal = signal(10)
+
+  constructor(){
+  }
+
+  increaseBy(value: number){
+    this.counter += value;
+    //this.counterSignal.set(this.counterSignal() + value)
+    this.counterSignal.update( curent => curent + value )
+  }
+
+  resetCounter(){
+    this.counter = 0;
+    this.counterSignal.set(0)
+  }
+
+}
